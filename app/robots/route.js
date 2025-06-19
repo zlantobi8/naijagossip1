d76f7f0c195cf579c2fbc78cc52a609dd293ffd2
zlantobi8 <fagbuyirotobi4@gmail.com>
2025-06-18 19:42:12 -0700

    message

--- app/robots/route.js
+++ app/robots/route.js
@@ -1,7 +1,6 @@
-// app/robots.txt/route.js
 export async function GET() {
   return new Response(
-    `User-agent: *\nAllow: /\nSitemap: https://naijagossip.vercel.app/sitemaps.xml`,
+    `User-agent: *\nAllow: /\nSitemap: https://naijagossip.vercel.app/sitemap.xml`,
     {
       headers: {
         'Content-Type': 'text/plain',
