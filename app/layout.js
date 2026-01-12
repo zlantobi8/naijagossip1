49fc66a5efd77a5ea7a16234822e7d456319f6ee
zlantobi8 <fagbuyirotobi4@gmail.com>
2026-01-12 07:28:39 +0100

    wwwwdwr

--- app/layout.js
+++ app/layout.js
@@ -76,57 +76,6 @@ export default function RootLayout({ children }) {
         {children}
 
         
-  <Script
-  id="popunder-ad"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-(function(){
-  var z=window,
-      l="dbf7f1a4e7f933586fb8573ab5e33727",
-      v=[
-        ["siteId",750*338-982+335+475+5014059],
-        ["minBid",0],
-        ["popundersPerIP","0"],
-        ["delayBetween",0],
-        ["default",false],
-        ["defaultPerDay",0],
-        ["topmostLayer","auto"]
-      ],
-      i=[
-        "d3d3LmludGVsbGlnZW5jZWFkeC5jb20vUUhFSU5uL3Vkb200Lmpz",
-        "ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvai9qTWtzL2ZqcXVlcnkuanFHcmlkLm1pbi5jc3M=",
-        "d3d3Lnhva2hsbXV3YWR5aHAuY29tL21zTi9nZG9tNC5qcw==",
-        "d3d3Lmdmb3NlZ2d1ZXZrLmNvbS94aHdDeC9WUkFzZGwvaWpxdWVyeS5qcUdyaWQubWluLmNzcw=="
-      ],
-      s=-1,d,k,
-      n=function(){
-        clearTimeout(k);
-        s++;
-        if(i[s] && !(1793857825000 < (new Date).getTime() && 1 < s)){
-          d=z.document.createElement("script");
-          d.type="text/javascript";
-          d.async=true;
-          var t=z.document.getElementsByTagName("script")[0];
-          d.src="https://"+atob(i[s]);
-          d.crossOrigin="anonymous";
-          d.onerror=n;
-          d.onload=function(){
-            clearTimeout(k);
-            z[l.slice(0,16)+l.slice(0,16)] || n()
-          };
-          k=setTimeout(n,5000);
-          t.parentNode.insertBefore(d,t)
-        }
-      };
-  if(!z[l]){
-    try{Object.freeze(z[l]=v)}catch(e){}
-    n()
-  }
-})();
-    `,
-  }}
-/>
 
 
       </body>
