e2313b0fbb98b096037091756841b84539679acd
zlantobi8 <fagbuyirotobi4@gmail.com>
2026-01-12 07:25:29 +0100

    wwwwdwr

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,35 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com', // allow Cloudinary fetch URLs
+        hostname: 'res.cloudinary.com',
       },
     ],
+  },
+  // Add this transpile block for vendor libraries
+  transpilePackages: ['owl.carousel', 'slick-carousel', 'line-awesome'],
+  
+  webpack: (config) => {
+    config.module.rules.forEach((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf.forEach((oneOf) => {
+          // Look for CSS loaders
+          if (oneOf.test && oneOf.test.toString().includes('css')) {
+            if (oneOf.use && Array.isArray(oneOf.use)) {
+              oneOf.use.forEach((loader) => {
+                // Disable url() resolving to ignore missing vendor assets
+                if (loader.loader && loader.loader.includes('css-loader')) {
+                  loader.options = { 
+                    ...loader.options, 
+                    url: false 
+                  };
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+    return config;
   },
 };
 
