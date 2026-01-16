9eed3c64dd01edd7094becfeb956c98586e17b0c
zlantobi8 <fagbuyirotobi4@gmail.com>
2026-01-16 10:06:10 +0100

    porr

--- app/welcome/page.js
+++ app/welcome/page.js
@@ -1,10 +1,10 @@
 'use client'
 import { useEffect, useState } from "react";
-import Banner from "../components/Banner";
+
+import HomePage from "../page";
 
 export default function Welcome() {
-  const [subscribed, setSubscribed] = useState(false);
-  const [progress, setProgress] = useState(0);
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -28,43 +28,9 @@ export default function Welcome() {
   };
 
   return (
-    <div style={{ position: "relative", height: "100vh", fontFamily: "Arial, sans-serif", overflow: "hidden" }}>
+    <>
+      <HomePage/>
+    </>
 
-      {/* Blurred image background */}
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          backgroundImage: 'url("https://pbs.twimg.com/media/G8N8hWTWoAIFjnS.jpg:large")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          filter: "blur(2px) brightness(0.6)", // subtle blur
-          zIndex: -1,
-        }}
-      />
-
-   
-        
-      <iframe
-        data-aa="2424319"
-        src="//acceptable.a-ads.com/2424319/?size=Adaptive"
-        style={{
-          border: 0,
-          padding: 0,
-          width: "70%",
-          height: "auto",
-          overflow: "hidden",
-          display: "block",
-          margin: "auto",
-        }}
-      ></iframe>
-
-
-        <Banner/>
-   
-    </div>
   );
 }
